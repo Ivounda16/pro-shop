e644c638030f097b5cddbc552eedec06d69876e1
Bruno James <kombilahogrel@gmail.com>
2020-12-19 16:27:44 +0000

    added the shopping cart, User Authentication on both our backend and frontend... also added a profile page to edit userInfo

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 
 const userSchema = mongoose.Schema({
@@ -21,6 +22,22 @@ const userSchema = mongoose.Schema({
     }
 }, {
     timestamps: true
+})
+
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+userSchema.pre('save', async function (next) {
+    // if it has been modified, it will run the hashed password
+    if(!this.isModified('password')) {
+        next()
+    }
+
+    // salt hashes password asynchronously. "10" is the number of rounds
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
 })
 
 
