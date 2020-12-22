5d6c7188070a1174521466290dc1a0c222cf95d4
Bruno James <kombilahogrel@gmail.com>
2020-12-22 23:10:30 +0000

    created the OrderScreen and implemented the option of knowing if the order items are paid and delivered

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 
 const App = () => {
@@ -21,9 +22,10 @@ const App = () => {
       <Header/>
       <main className='py-3'>
         <Container>
-          <Route path='/placeorder' component={PlaceOrderScreen}/>
           <Route path='/payment' component={PaymentScreen}/>
           <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/placeorder' component={PlaceOrderScreen}/>
+          <Route path='/order/:id' component={OrderScreen}/>
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
           <Route path='/profile' component={ProfileScreen}/>
