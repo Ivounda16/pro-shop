6d8fd68a6ec68c378367d9481cb622301878d914
Bruno James <kombilahogrel@gmail.com>
2020-12-22 23:23:06 +0000

    added the paid status controller and route in our backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -63,5 +63,32 @@ const getOrderById = asyncHandler(async (req, res) => {
 
 
 
+//  @desc Update order to paid 
+// @route GET /api/orders/:id/pay
+// @access Private
 
-export { addOrderItems, getOrderById }
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id)
+
+    if(order) {
+        order.isPaid = true
+        order.paidAt = Date.now()
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address
+        }
+
+
+        const updatedOrder = await order.save()
+
+        res.json(updatedOrder)
+    } else {
+        throw new Error('Order not found')
+    }
+})
+
+
+
+export { addOrderItems, getOrderById, updateOrderToPaid }
